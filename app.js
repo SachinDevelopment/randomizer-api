684e95f22dc0c08c6c433b260c2abef013bdad9a
SachinDevelopment <53714757+SachinDevelopment@users.noreply.github.com>
2021-01-07 19:19:38 -0500

    add games end point

--- app.js
+++ app.js
@@ -14,54 +14,12 @@ app.get('/players', async (req, res) => {
     try {
         // establish a connection to MariaDB
         conn = await pool.getConnection();
-
         // create a new query
         var query = "select * from players";
-
         // execute the query and set the result to a new variable
         var rows = await conn.query(query);
-
         // return the results
         res.send(rows);
-    } catch (err) {
-        throw err;
-    } finally {
-        if (conn) return conn.release();
-    }
-});
-
-
-app.get('/win/:id', async (req, res) => {
-    const { id } = req.params;
-    let conn;
-    try {
-        // establish a connection to MariaDB
-        conn = await pool.getConnection();
-        // create a new query
-        var query = `UPDATE players SET wins=wins+1 WHERE id=${id}`;
-        // execute the query and set the result to a new variable
-        await conn.query(query);
-        // return the results
-        res.sendStatus(200);
-    } catch (err) {
-        throw err;
-    } finally {
-        if (conn) return conn.release();
-    }
-});
-
-app.get('/lose/:id', async (req, res) => {
-    const { id } = req.params;
-    let conn;
-    try {
-        // establish a connection to MariaDB
-        conn = await pool.getConnection();
-        // create a new query
-        var query = `UPDATE players SET loses=loses+1 WHERE id=${id}`;
-        // execute the query and set the result to a new variable
-        await conn.query(query);
-        // return the results
-        res.sendStatus(200);
     } catch (err) {
         throw err;
     } finally {
@@ -74,9 +32,9 @@ app.get('/reset', async (req, res) => {
     try {
         // establish a connection to MariaDB
         conn = await pool.getConnection();
-        var query = `UPDATE players SET loses=0`;
+        var query = `UPDATE players_test SET loses=0`;
         await conn.query(query);
-        var query = `UPDATE players SET wins=0`;
+        var query = `UPDATE players_test SET wins=0`;
         await conn.query(query);
         // return the results
         res.sendStatus(200);
@@ -87,6 +45,58 @@ app.get('/reset', async (req, res) => {
     }
 });
 
+app.get('/stats/:id', async (req, res) => {
+    const { id } = req.params;
+
+    let conn;
+    try {
+        // establish a connection to MariaDB
+        conn = await pool.getConnection();
+        var query = `SELECT id, name FROM players_test where id = ${id}`;
+        const me = await conn.query(query);
+        query = `SELECT id, name FROM players_test where id != ${id}`;
+        const others = await conn.query(query);
+        const func = () => {
+            const promises = others.map(async (player) => {
+                query = `SELECT count(*) as count from (SELECT * FROM games WHERE winners LIKE "%${me[0].name}-${me[0].id}%") AS sub WHERE winners LIKE "%${player.name}-${player.id}%";`;
+                const wincount = await conn.query(query);
+                query = `SELECT count(*) as count from (SELECT * FROM games WHERE losers LIKE "%${me[0].name}-${me[0].id}%") AS sub WHERE losers LIKE "%${player.name}-${player.id}%";`;
+                const losecount = await conn.query(query);
+                return { player: player.name, wins: wincount[0].count, loses: losecount[0].count };
+            });
+            return Promise.all(promises);
+        }
+        const results = await func();
+        return res.send(results);
+    } catch (err) {
+        throw err;
+    } finally {
+        if (conn) return conn.release();
+    }
+});
+
+app.post('/games', async (req, res) => {
+    const { game_size, winners, losers, winning_side, winnerIds, loserIds } = req.body;
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        var query = `INSERT INTO games (game_size, winning_side, winners, losers) VALUES (${game_size}, "${winning_side}", "${winners}", "${losers}");`;
+        const r = await conn.query(query);
+        var query = `UPDATE players_test SET wins=wins+1 WHERE id in (${winnerIds});`;
+        await conn.query(query);
+        var query = `UPDATE players_test SET loses=loses+1 WHERE id in (${loserIds});`;
+        await conn.query(query);
+        var query = "select * from players_test";
+        const result = await conn.query(query);
+        // return the results
+        res.send(result);
+    } catch (err) {
+        throw err;
+    } finally {
+        if (conn) return conn.release();
+    }
+});
+
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Randomizer-api listening at http://localhost:${port}`);
 });
